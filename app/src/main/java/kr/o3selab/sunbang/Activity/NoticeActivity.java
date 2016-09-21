e4bb4b72d188f52064cd811846583fb1c3cb00af
duveen@duveen.me <ebqls1234>
2016-09-22 01:07:44 +0900

    New Commit

--- app/src/main/java/kr/o3selab/sunbang/Activity/NoticeActivity.java
+++ app/src/main/java/kr/o3selab/sunbang/Activity/NoticeActivity.java
@@ -2,7 +2,6 @@ package kr.o3selab.sunbang.Activity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
@@ -13,13 +12,6 @@ import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.Charset;
 
 import kr.o3selab.sunbang.Instance.DB;
 import kr.o3selab.sunbang.Instance.JsonHandler;
